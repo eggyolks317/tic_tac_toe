95f8858bd73a99a96b6ecf7402982e25c9f32dda
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-11-24 20:29:51 -0800

    added board display for visualization

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+let displayBoard = document.getElementById("board");
 function createUser(name) {
   return { name };
 }
 
 function createBoard() {
   let board = [
-    [, , ,],
-    [, , ,],
-    [, , ,],
+    [, ,],
+    [, ,],
+    [, ,],
   ];
+  for (let i = 0; i < 9; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    displayBoard.appendChild(cell);
+  }
   return { board };
 }
+
+let board = createBoard();
