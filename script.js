c242661d93b2f4fa101d1086fbd5a5f45efc7b76
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-11-24 20:14:50 -0800

    added createBoard function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function createUser(name) {
   return { name };
 }
+
+function createBoard(board) {
+  return { board };
+}
+
+console.log(
+  createBoard([
+    [, , ,],
+    [, , ,],
+    [, , ,],
+  ])
+);
